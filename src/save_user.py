31b375792170fee5301bb69808af102e3213ca58
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-05-05 18:20:56 -0500

    Saving the ids

--- src/save_user.py
+++ src/save_user.py
@@ -1,11 +1,16 @@
 from telethon import TelegramClient
 
+user_id_txt = open("user_id.txt" , "a")
+
 async def get_members(client: TelegramClient):
     try:
         channel = await client.get_entity(-1001196822184)
-        i = 0
+        i = 1
         async for user in client.iter_participants(channel):
-            # i+1 
             print(f"{i} -{user.id} - {user.first_name} - {user.username}")
+            user_id_txt.write(f"{user.id}")
+            i += 1
+        user_id_txt.close()
+
     except Exception as e:
         print(f"Error fetching members: {e}")
