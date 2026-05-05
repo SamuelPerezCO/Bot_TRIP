efdd973a5925ccf8fedb5880bb96370c9f8512e9
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-05-05 13:48:52 -0500

    send a message

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from telethon import TelegramClient
+from telethon import TelegramClient , events , sync
 from dotenv import load_dotenv
 import sys
 import os
@@ -8,4 +8,19 @@ load_dotenv()
 api_id = os.getenv("api_id")
 api_hash = os.getenv("api_hash")
 
-client = TelegramClient('bot_trip' , api_id , api_hash)
+client = TelegramClient('TRIP_UNP_BOT' , api_id = api_id , api_hash = api_hash)
+client.start()
+
+print(client.get_me().stringify())
+
+
+client.send_message('@unaneaprogramadora' , 'Hello! Talking to you from Telethon')
+client.send_file('@unaneaprogramadora' , '/home/samuelp/Pictures/UNP/ChatGPT Image Feb 5, 2026, 10_35_16 PM.png')
+
+client.download_profile_photo('me')
+messages = client.get_messages('@unaneaprogramadora')
+messages[0].download_media()
+
+@client.on(events.NewMessage(pattern='(?i)hi|hello'))
+async def handler(event):
+    await event.respond('Hey!')
