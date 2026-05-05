671a202a5070e683025ab393e7a94e121f1badaa
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-05-05 17:42:17 -0500

    getting the users

--- src/save_user.py
+++ src/save_user.py
@@ -1,11 +1,11 @@
 from telethon import TelegramClient
-from telethon.tl.types import ChannelParticipantsSearch
-from get_client import client
 
-async def get_members():
-    async with client:
-        channel = await client.get_entity('Estudiantes Academia de Idiomas Smart')
-        participants = await client.get_participants(channel)
-
-        for user in participants:
-            print(user.id , user.first_name , user.username)
+async def get_members(client: TelegramClient):
+    try:
+        channel = await client.get_entity(-1001196822184)
+        i = 0
+        async for user in client.iter_participants(channel):
+            # i+1 
+            print(f"{i} -{user.id} - {user.first_name} - {user.username}")
+    except Exception as e:
+        print(f"Error fetching members: {e}")
